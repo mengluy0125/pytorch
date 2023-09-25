5a869225060e7310df01334308bb53e07293e75d
Rodrigo Kumpera <kumpera@meta.com>
2023-09-25 11:33:06 -0700

    [C10] PG observability hooks.
    Expose a set of observability hooks into C10D such that our users can
    detect collectives failure both faster and more easily.
    
    The design is similar to NCCL desync debug that it minimized the
    overhead by doing most of the work out of the main thread.
    
    This PR introduces a new module torch.distributed.hooks that exposes the following set of methods:
    
        register_collective_start_hook
        register_collective_end_hook
        register_process_group_hook
    
    The process group hook exposes PG creation on the member ranks and call them inline from the
    the PG creation code. This is fine since this happens during initialization and a limited number of times.
    
    The collective start/end hooks are fired from a single background thread. It reads
    events from a C++ queue and dispatches over.
    
    Queue notification is oddly done using a pipe, this is needed so python can abort the thread on shutdown
    and have it as background thread. This is not possible with more reasonable choices like a condvar.
    
    ghstack-source-id: 57caff5e512826f1f0a3c2d5de77a2f73d26c100
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108815

--- torch/csrc/distributed/c10d/Backend.cpp
+++ torch/csrc/distributed/c10d/Backend.cpp
@@ -1,6 +1,8 @@
 #include <c10/util/Logging.h>
 #include <fmt/format.h>
 #include <torch/csrc/distributed/c10d/Backend.hpp>
+#include <torch/csrc/distributed/c10d/Hooks.hpp>
+#include <torch/csrc/distributed/c10d/logging.h>
 
 namespace c10d {
 
@@ -13,6 +15,40 @@ Backend::~Backend() = default;
 
 void Backend::init() {
   C10_LOG_API_USAGE_ONCE(fmt::format("c10d.backend_{}", getBackendName()));
+}
+
+void commonEventinit(
+    details::EventInfo& evt,
+    const Backend& backend,
+    const Work& work) {
+  evt.timestamp =
+      std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  evt.pg_name = backend.getGroupName();
+  evt.backend = backend.getBackendName();
+  evt.sequence_number = work.getSequencenumber();
+  evt.operation = c10d::opTypeToString(work.retrieveOpType());
+}
+
+void Backend::emitCollectiveStart(const Work& work) {
+  details::EventInfo evt;
+  commonEventinit(evt, *this, work);
+
+  evt.event_kind = details::EventKind::CollectionStart;
+  details::enqueue_c10d_event(std::move(evt));
+}
+
+void Backend::emitCollectiveEnd(const Work& work) {
+  details::EventInfo evt;
+  commonEventinit(evt, *this, work);
+
+  evt.event_kind = details::EventKind::CollectionEnd;
+  // FIXME change getDuration to return Optional<float>
+  try {
+    evt.duration_ms = work.getDuration();
+  } catch (std::exception& e) {
+    C10D_INFO("Duraction not available {}", e.what());
+  }
+  details::enqueue_c10d_event(std::move(evt));
 }
 
 } // namespace c10d
